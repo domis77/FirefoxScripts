784717f2931ffd08beda8d6748492ceee84bc5b1
Dominik <domis77@vp.pl>
2017-05-01 02:50:22 +0200

    readability&En_lang_button

---
+++ wikipedia_CSS.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        wikipedia
+// @namespace   wikipedia
+// @description wikipedia_CSS
+// @include     https://pl.wikipedia.org/
+// @version     1
+// @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+/* add font */
+$("head").prepend("<style type=\"text/css\">" + 
+                                "@font-face {\n" +
+                                    "\tfont-family: \"SanFrancisco\";\n" + 
+                                    "\tsrc: local('¢'), url('https://applesocial.s3.amazonaws.com/assets/styles/fonts/sanfrancisco/sanfranciscodisplay-regular-webfont.woff') format('opentype');\n" + 
+                                "}\n" + 
+                            "</style>");
+
+/* style content */
+$('.mw-body-content').css({
+ "font-size": "17px",
+	"line-height": "1.52947",
+	"font-weight": "400",
+	"letter-spacing": "-0.021em",
+  "font-family": '"SanFrancisco", "Tahoma", "Arial", sans-serif',
+	"color": "#666",
+	"font-style": "normal"  
+});
+
+
+/* style english language button */
+$('.interwiki-en').css({
+  "background-color": "#c6c6c6"
+});
+
+$('.interwiki-en').children().css({
+   "color": "red",
+   "margin-left": "25%",
+   "font-family": '"SanFrancisco",sans-serif"',
+   "font-size": "140%"
+});
+
